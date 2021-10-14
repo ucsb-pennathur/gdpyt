34ee2d23d0ec939abafaa2f063d79e37819a7e67
sean mackenzie <sean_mackenzie@ucsb.edu>
2021-10-14 10:03:38 -0700

    trying to remove auto-plotting causing Google Colab to fail

--- tests/test_dataset_unpacker.py
+++ tests/test_dataset_unpacker.py
@@ -12,7 +12,7 @@ import numpy as np
 from skimage.morphology import disk, square
 
 # ----- ----- ----- ----- TEST DATASET UNPACKER ----- ----- ----- ----- ----- ----- -----
-test_dataset = '10.07.21-BPE_Pressure_Deflection'
+test_dataset = '10X_0.5Xmag_2.15umNR_HighInt_0.03XHg'
 
 if test_dataset == '10.07.21-BPE_Pressure_Deflection':
     calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration', static_templates=False,
@@ -27,24 +27,32 @@ elif test_dataset == 'JP-EXF01-20':
                                      number_of_images=60, particle_distribution='Dataset_I',
                                      particle_density='361', static_templates=False).unpack()
 elif test_dataset == 'synthetic_overlap_noise-level':
-    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration', noise_level=1,
-                                      particle_distribution='grid', particle_density='10e-3',
-                                      static_templates=False).unpack()
-    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test', noise_level=1,
-                                     particle_distribution='grid', particle_density='10e-3',
+    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration', noise_level=2,
+                                      particle_distribution='random', particle_density='1e-3',
+                                      static_templates=True).unpack()
+    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test', noise_level=2,
+                                     particle_distribution='random', particle_density='1e-3',
                                      static_templates=True).unpack()
 elif test_dataset == '20X_1Xmag_5.61umPink_HighInt':
-    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration').unpack()
-    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test').unpack()
+    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration',
+                                      static_templates=True, single_particle_calibration=False, hard_baseline=True).unpack()
+    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test',
+                                     static_templates=True, single_particle_calibration=False, hard_baseline=True).unpack()
 elif test_dataset == '10X_0.5Xmag_2.15umNR_HighInt_0.03XHg':
-    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration').unpack()
-    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test').unpack()
+    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration',
+                                      static_templates=True, single_particle_calibration=False, hard_baseline=True).unpack()
+    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test',
+                                     static_templates=True, single_particle_calibration=False, hard_baseline=True).unpack()
 elif test_dataset == '10X_1Xmag_5.1umNR_HighInt_0.06XHg':
-    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration').unpack()
-    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test').unpack()
+    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration',
+                                      static_templates=True, single_particle_calibration=False, hard_baseline=True).unpack()
+    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test',
+                                     static_templates=True, single_particle_calibration=False, hard_baseline=True).unpack()
 elif test_dataset == '10X_1Xmag_2.15umNR_HighInt_0.12XHg':
-    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration').unpack()
-    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test').unpack()
+    calib_settings = dataset_unpacker(dataset=test_dataset, collection_type='calibration',
+                                      static_templates=False, single_particle_calibration=False, hard_baseline=True).unpack()
+    test_settings = dataset_unpacker(dataset=test_dataset, collection_type='test',
+                                     static_templates=False, single_particle_calibration=False, hard_baseline=True).unpack()
 
 
 # ----- ----- ----- ----- TEST ----- ----- ----- ----- ----- ----- -----
